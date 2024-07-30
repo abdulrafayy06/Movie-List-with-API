1baa6d0afd92509f1f793de57b89ee26b8eb2ca3
Abdul Rafay <lifewithabdulrafay@gmail.com>
2024-07-30 12:52:43 +0500

    make search bar and remove extra files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import React, {useEffect} from "react";
+import "./App.css";
+import "./MovieCard.jsx";
+import SearchIcon from "./search.svg";
+
+
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  useEffect(() => { 
-    alert('Welcome to Counter App');
-  });
-  return (
-    <div className="App">
-      <h1>Counter App</h1>
-      <h2>{count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-    </div>
-  );
-}
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies("Spiderman");
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Movies Land</h1>
+            <div className="search">
+                <input onChange={()=> {}} placeholder="Search for movies"/>
+                <img 
+                src= {SearchIcon} 
+                alt="search"
+                onClick={() => {}}
+                />
+            </div>
+        </div>
+    );
+    }
 
 export default App;
